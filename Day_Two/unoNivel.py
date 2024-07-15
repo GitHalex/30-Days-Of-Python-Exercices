65bbb2c56191472b6469022d327469882771ee1c
GitHalex <calex1731@gmail.com>
2024-07-15 19:22:45 -0400

    Dia 2 completamente terminado

---
+++ Day_Two/unoNivel.py
@@ -0,0 +1,25 @@
+# Day 2: 30 Days of python programming
+
+miNombre: str = "Alex"
+miApellido: str = "Lopez Perez"
+miNombreCompleto: str = "Alex Lopez Perez"
+myCountry: str = "Bolivia"
+myCity: str = "Chacobamba"
+age: int = 20
+year: int = 2024
+isMarried: bool = True
+isTrue: bool = False
+isLight: bool = True
+
+print(type(miNombre))
+print(type(miApellido))
+print(type(miNombreCompleto))
+print(type(myCountry))
+print(type(age))
+print(type(year))
+print(type(isMarried))
+print(type(isTrue))
+print(type(isLight))
+
+print(f"tamaño de mi nombre: {miNombre} => {len(miNombre)}")
+print(f"{len(miNombre) == len(miApellido)}")
