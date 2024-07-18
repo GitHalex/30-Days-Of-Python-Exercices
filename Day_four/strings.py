2b561c6f1c071c1b663cf9f4c2e2de8704932318
GitHalex <calex1731@gmail.com>
2024-07-17 22:46:02 -0400

    Ejercicio de dia 4 estasn casi terminado

---
+++ Day_four/strings.py
@@ -0,0 +1,14 @@
+palabra1 = "Thirty"
+palabra2 = "Days"
+palabra3 = "Of"
+palabra4 = "Python"
+
+respuesta = palabra1 + " " + palabra2 + " " + palabra3 + " " + palabra4
+print(respuesta)
+
+company = "Coding for All"
+print(f"Variable: {company}")
+print(f"tamaño: {len(company)}")
+print(f"En MAYUSCULAS: {company.upper()}")
+print(f"EN MINUSCULAS: {company.lower()}")
+print(f"capitalize: {company.capitalize()} => title: {company.title()} => swapcase: {company.swapcase()}")
