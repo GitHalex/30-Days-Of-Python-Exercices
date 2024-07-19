f75a3458652f7ed4b207aaabf85322794d940a3f
GitHalex <calex1731@gmail.com>
2024-07-19 13:58:13 -0400

    Ejercicios de nivel uno estan completamente terminado

---
+++ Day_six/tuplas.py
@@ -0,0 +1,20 @@
+tuplavacia = ()
+print(tuplavacia)
+print(type(tuplavacia))
+
+namesTuplaFemale = ("Ana", "Juana", "Chanel")
+namesTuplaMale = ("Alex","Juan", "Chanel")
+
+brothersTupla = namesTuplaFemale + namesTuplaMale
+print(brothersTupla)
+
+siblings = brothersTupla.count("Chanel")
+print(f"Tengo: {siblings} hermanos gemelos")
+
+family_members = list(brothersTupla)
+family_members.append("Father")
+family_members.append("Mother")
+
+print(family_members)
+enTuplaMiembros = tuple(family_members)
+print(enTuplaMiembros)
