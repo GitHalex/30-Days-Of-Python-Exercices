616a97ac4b026218a686e4a50c554ad5ab9669bb
GitHalex <calex1731@gmail.com>
2024-07-19 20:57:47 -0400

    Ejercicio completamente terminado del dia 6

--- Day_six/tuplas.py
+++ Day_six/tuplas.py
@@ -27,3 +27,25 @@ print(cd)
 print(e)
 print(h)
 
+fruits = ("Platano", "Fresa", "Tomate")
+vegetables = ("Papa", "Lechuga")
+animal_products= ("Carna", "Pollo")
+
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+
+listaFood = list(food_stuff_tp)
+mitad = len(listaFood) // 2
+print(listaFood)
+
+mitadFoods = listaFood[0: mitad]
+print(mitadFoods)
+ultimosTres = food_stuff_tp[-3:]
+print(ultimosTres)
+
+del food_stuff_tp
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
