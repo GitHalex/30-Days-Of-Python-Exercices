774a38adc09ea830d9aa8d06840632bf09565813
GitHalex <calex1731@gmail.com>
2024-07-19 21:54:05 -0400

    Ejercicios del nivel uno completamente terminado

--- Day_seven/sets.py
+++ Day_seven/sets.py
@@ -7,3 +7,14 @@ age = [22, 19, 24, 25, 26, 24, 25, 24]
 print(len(it_companies))
 it_companies.add("Twitter")
 print(it_companies)
+
+others_companies = {"Meta", "Oracle"}
+it_companies.update(others_companies)
+print(it_companies)
+
+guardado = it_companies.remove("Facebook")
+print(guardado)
+print(it_companies)
+
+print(it_companies.discard("IBM"))
+print(it_companies)
