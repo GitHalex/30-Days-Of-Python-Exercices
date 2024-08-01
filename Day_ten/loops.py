7d7a99e22641b65465883a0e69c0880f637eada0
GitHalex <calex1731@gmail.com>
2024-07-31 22:11:13 -0400

    Ejercicio 2 del nivel 2 estan completamente terminado

--- Day_ten/loops.py
+++ Day_ten/loops.py
@@ -7,3 +7,13 @@ i = 0
 while i <= 10:
   print(i)
   i += 1
+print("Esto es al revez")
+for i in range(10, -1, -1):
+  print(i)
+
+print("Esto es con while")
+
+i = 10
+while i >= 0:
+  print(i)
+  i -= 1
