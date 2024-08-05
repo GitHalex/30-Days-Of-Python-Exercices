c09120a56aa7206065e6d6c22949210e675c89b2
GitHalex <calex1731@gmail.com>
2024-08-05 16:33:12 -0400

    Agregando las funciones que nos van a tratar

--- Day_eleven/funciones.py
+++ Day_eleven/funciones.py
@@ -2,3 +2,18 @@ def add_two_numbers(num1: int, num2: int) -> int:
   return num1 + num2
 
 print(add_two_numbers(2, 2))
+print(add_two_numbers("2", "3"))
+####################
+
+from math import pi
+
+def areaOfCircle(radio: int) -> float:
+  area = pi*radio**2
+  return round(area, 2)
+
+print(areaOfCircle(2))
+##########################
+
+
+
+
