707eb3d068db868f0e000192b2bc5c8fd875f614
GitHalex <calex1731@gmail.com>
2024-08-23 13:40:53 -0400

    Ejercicio donde ingresamos un numero map y filter

--- Day_fourteen/higher_order.py
+++ Day_fourteen/higher_order.py
@@ -2,44 +2,51 @@ countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
 names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for pais in countries:
+usandoDistintasListas = filter(lambda pais: len(pais) == 6, map(lambda pais: pais.upper(), countries))
+print(list(usandoDistintasListas))
+
+
+
+""" for pais in countries:
   print(pais)
 
 for name in names:
   print(name)
 
 for number  in numbers:
-  print(number)
+  print(number) """
 
-mayusculas = list(map(lambda pais: pais.upper(), countries))
-print(mayusculas)
+""" mayusculas = list(map(lambda pais: pais.upper(), countries))
+print(mayusculas) """
 
 
-square = map(lambda number: number ** 2, numbers)
-print(list(square))
+""" square = map(lambda number: number ** 2, numbers)
+print(list(square)) """
 
-def namesMayuscula(name):
+""" def namesMayuscula(name):
   return name.upper()
 
 namesMayusculas = map(namesMayuscula, names)
-print(list(namesMayusculas))
+print(list(namesMayusculas)) """
 
 
-def funcionFilter(pais):
+""" def funcionFilter(pais):
   if "land" in pais:
     return True
   
 countriesLand = filter(funcionFilter, countries)
-print(list(countriesLand))
+print(list(countriesLand)) """
 
-otro = list(filter(lambda pais: "land" in pais, countries))
-print(otro)
+""" otro = list(filter(lambda pais: "land" in pais, countries))
+print(otro) """
 
-soloSeis = list(filter(lambda pais: len(pais) == 6, countries))
-print(soloSeis)
+""" soloSeis = list(filter(lambda pais: len(pais) == 6, countries))
+print(soloSeis) """
 
-masSeis = list(filter(lambda pais: len(pais) >= 7, countries))
-print(masSeis)
+""" masSeis = list(filter(lambda pais: len(pais) >= 7, countries))
+print(masSeis) """
 
-empieza_E = list(filter(lambda pais: "E" in pais, countries))
-print(empieza_E)
+""" empieza_E = list(filter(lambda pais: "E" in pais, countries))
+print(empieza_E) """
+
+
