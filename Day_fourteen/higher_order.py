7720d56ed864d80aab79d30d4d026a8cd276c103
GitHalex <calex1731@gmail.com>
2024-08-22 21:51:01 -0400

    Ejercicios del primer nivel estan completamente terminado

--- Day_fourteen/higher_order.py
+++ Day_fourteen/higher_order.py
@@ -1,92 +1,16 @@
-def sum_numbers(nums):  # normal function
-    return sum(nums)    # a sad function abusing the built-in sum function :<
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def higher_order_function(f, lst):  # function as a parameter
-    summation = f(lst)
-    return summation
-result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
-print(result)       # 15
+for pais in countries:
+  print(pais)
 
+for name in names:
+  print(name)
 
-def square(x):          # a square function
-    return x ** 2
+for number  in numbers:
+  print(number)
 
-def cube(x):            # a cube function
-    return x ** 3
+mayusculas = list(map(lambda pais: pais.upper(), countries))
+print(mayusculas)
 
-def absolute(x):        # an absolute value function
-    if x >= 0:
-        return x
-    else:
-        return -(x)
-
-def higher_order_function(type): # a higher order function returning a function
-    if type == 'square':
-        return square
-    elif type == 'cube':
-        return cube
-    elif type == 'absolute':
-        return absolute
-
-result = higher_order_function('square')
-print(result(3))       # 9
-result = higher_order_function('cube')
-print(result(3))       # 27
-result = higher_order_function('absolute')
-print(result(-3))      # 3
-
-
-# Closures
-
-def add_ten():
-    ten = 10
-    def add(num):
-        return num + ten
-    return add
-
-closure_result = add_ten()
-print(closure_result(5))  # 15
-print(closure_result(10))  # 20
-
-
-
-'''These decorator functions are higher order functions
-that take functions as parameters'''
-
-# First Decorator
-def uppercase_decorator(function):
-    def wrapper():
-        func = function()
-        make_uppercase = func.upper()
-        return make_uppercase
-    return wrapper
-
-# Second decorator
-def split_string_decorator(function):
-    def wrapper():
-        func = function()
-        splitted_string = func.split()
-        return splitted_string
-
-    return wrapper
-
-@split_string_decorator
-@uppercase_decorator     # order with decorators is important in this case - .upper() function does not work with lists
-def greeting():
-    return 'Welcome to Python'
-print(greeting())   # WELCOME TO PYTHON
-
-
-
-def decorator_with_parameters(function):
-    def wrapper_accepting_parameters(para1, para2, para3):
-        function(para1, para2, para3)
-        print("I live in {}".format(para3))
-    return wrapper_accepting_parameters
-
-@decorator_with_parameters
-def print_full_name(first_name, last_name, country):
-    print("I am {} {}. I love to teach.".format(
-        first_name, last_name, country))
-
-print_full_name("Asabeneh", "Yetayeh",'Finland')
